3ccc7b3577eb045b1abd4fa8c70f5e2e96111062
BuildTools <unconfigured@null.spigotmc.org>
2019-03-28 21:00:57 -0300

    Added help command

--- bitrate.hs
+++ bitrate.hs
@@ -4,5 +4,6 @@ good = 0.18
 nice = 0.22
 excellent = 0.27
 
+help = "Bitrate Calculator = calculateBitrate length height frames quality" ++ "                                     " ++ "Quality Calculator = calculateQuality length height frames bitrate"
 calculateBitrate length height frames quality = fromInteger length * fromInteger height * fromInteger frames * quality / 1000.0
 calculateQuality length height frames bitrate = (fromInteger bitrate * 1000.0) / fromInteger frames / fromInteger height / fromInteger length
